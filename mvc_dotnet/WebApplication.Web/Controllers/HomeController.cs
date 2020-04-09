d1cb7d387effa76a77f25e9cdb87336666ab4354
Lauren Miles <lrnmls@me.com>
2020-04-09 15:47:36 -0400

    Saving CSS updates

--- mvc_dotnet/WebApplication.Web/Controllers/HomeController.cs
+++ mvc_dotnet/WebApplication.Web/Controllers/HomeController.cs
@@ -79,7 +79,6 @@ namespace WebApplication.Web.Controllers
             List<Team> teams = teamDAL.GetTeamsByLeague(League);
             return View(teams);
         }
-<<<<<<< HEAD
 
         //private Team AddTeamNames(Team model)
         //{
@@ -97,15 +96,14 @@ namespace WebApplication.Web.Controllers
         //    selectListItems = new SelectListItem { Text = teamName, Value = teamName };
         //    return selectListItems;
         //}
-=======
-        
+       
         private SelectListItem AddTeamToList(string teamName)
         {
             SelectListItem selectListItems = new SelectListItem();
             selectListItems = new SelectListItem { Text = teamName, Value = teamName };
             return selectListItems;
         }
->>>>>>> 5e39ef46a6457fa64a7123676c18b90c8e199cce
+
 
         [HttpGet]
         [AuthorizationFilter("User")]
@@ -135,21 +133,13 @@ namespace WebApplication.Web.Controllers
             user = userDAL.GetUser(user.Username);
             return View(user);
         }
-<<<<<<< HEAD
 
-=======
-        
->>>>>>> 5e39ef46a6457fa64a7123676c18b90c8e199cce
         [HttpPost]
         [AuthorizationFilter("User")]
         public IActionResult UpdateUserInfo(User user, string Salt, string NewPassword, string Password)
         {
             TempData["Added"] = "Successfully updated username/password!";
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> 5e39ef46a6457fa64a7123676c18b90c8e199cce
             if (ModelState.IsValid)
             {
                 user = authProvider.GetCurrentUser();
@@ -158,7 +148,6 @@ namespace WebApplication.Web.Controllers
             }
             return View(user);
         }
-<<<<<<< HEAD
 
         //[HttpGet]
         //[AuthorizationFilter("Admin")]
@@ -170,7 +159,6 @@ namespace WebApplication.Web.Controllers
         //    {
         //        model.DropDownListTeam.Add(AddTeamToList(team.Name));
         //    }
-=======
         
         [HttpGet]
         [AuthorizationFilter("Admin")]
@@ -182,10 +170,8 @@ namespace WebApplication.Web.Controllers
             {
                 model.DropDownListTeam.Add(AddTeamToList(team.Name));
             }
->>>>>>> 5e39ef46a6457fa64a7123676c18b90c8e199cce
-
-        //    return View(model);
-        //}
+            return View(model);
+        }
 
         [HttpPost]
         [AuthorizationFilter("Admin")]
@@ -194,11 +180,7 @@ namespace WebApplication.Web.Controllers
             teamDAL.UpdateTeam(team);
             return View(team);
         }
-<<<<<<< HEAD
 
-=======
-        
->>>>>>> 5e39ef46a6457fa64a7123676c18b90c8e199cce
         public ActionResult Calendar()
         {
             //https://localhost:44392/home/calendar
