e4bc7bf2745faa86efec6790b1c75c8f99dc8859
Madi Kohr <madi.kohr@gmail.com>
2020-04-09 16:50:40 -0400

    Save

--- mvc_dotnet/WebApplication.Web/Models/Messages/MessagesModel.cs
+++ mvc_dotnet/WebApplication.Web/Models/Messages/MessagesModel.cs
@@ -7,6 +7,9 @@ namespace WebApplication.Web.Models.Messages
 {
     public class MessagesModel
     {
-
+        public int ID { get; set; }
+        public int SentByID { get; set; }
+        public int SentToID { get; set; }
+        public string MessageBody { get; set; }
     }
 }
