5eb7c43ea36d209af9585f47fcc53781dcb69fcf
Cody Theis <cody.theis@hotmail.com>
2020-04-14 12:41:23 -0400

    Sorting start

--- mvc_dotnet/schema.sql
+++ mvc_dotnet/schema.sql
@@ -358,6 +358,7 @@ INSERT INTO Messages(SentByUserID, toUserID, messageBody) VALUES (5,1, 'Detroit 
 INSERT INTO Messages(SentByUserID, toUserID, messageBody) VALUES (6,1, 'Wisconsin Rugby would like to play you on 09/11/2020 at Lambeau Field')
 INSERT INTO Messages(SentByUserID, toUserID, messageBody) VALUES (2,1, 'Chicago Rugby would like to play you on 09/12/2020 at Solider Field')
 INSERT INTO Messages(SentByUserID, toUserID, messageBody) VALUES (2,3, 'Chicago Rugby would like to play you on 09/12/2020 at Solider Field')
+INSERT INTO Messages(SentByUserID, toUserID, messageBody, userAccepted) VALUES (2,3, 'Chicago Rugby would like to play you on 09/12/2020 at Solider Field', 'Accepted')INSERT INTO Messages(SentByUserID, toUserID, messageBody, userAccepted) VALUES (2,4, 'Chicago Rugby would like to play you on 09/19/2020 at Solider Field', 'Accepted')
 
 
 COMMIT TRANSACTION;
