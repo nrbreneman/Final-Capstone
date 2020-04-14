6361585a246b4c724620ae16f438151df66a9966
Nathan Breneman <nrbreneman@gmail.com>
2020-04-14 13:14:14 -0400

    Added View Schedule to Admin

--- mvc_dotnet/WebApplication.Web/Controllers/HomeController.cs
+++ mvc_dotnet/WebApplication.Web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace SportsClubOrganizer.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Whoop()
+        {
+            return View();
+        }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
