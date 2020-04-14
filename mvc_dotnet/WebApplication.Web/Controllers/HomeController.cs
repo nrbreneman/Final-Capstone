7037d4a225b5115a2f50855b8db775745c308394
Cody Theis <cody.theis@hotmail.com>
2020-04-14 15:01:36 -0400

    Refact

--- mvc_dotnet/WebApplication.Web/Controllers/HomeController.cs
+++ mvc_dotnet/WebApplication.Web/Controllers/HomeController.cs
@@ -33,7 +33,7 @@ namespace SportsClubOrganizer.Web.Controllers
         {
             return View();
         }
-        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
