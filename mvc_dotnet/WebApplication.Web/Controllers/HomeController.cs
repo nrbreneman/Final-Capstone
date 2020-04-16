d552b03f02c5a83a359a5a6fe447f24b3ed098d5
Madi Kohr <madi.kohr@gmail.com>
2020-04-16 09:05:43 -0400

    MK 9:05

--- mvc_dotnet/WebApplication.Web/Controllers/HomeController.cs
+++ mvc_dotnet/WebApplication.Web/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ namespace SportsClubOrganizer.Web.Controllers
             return View();
         }
 
+        public IActionResult TeamNovember()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
